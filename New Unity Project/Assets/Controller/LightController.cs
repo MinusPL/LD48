679fcd5bc6e556f00ad5c988277b5f71af21f099
Minus <theminusoid@gmail.com>
2021-04-24 20:33:09 +0200

    Added light controller.

---
+++ New Unity Project/Assets/Controller/LightController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightController : MonoBehaviour
+{
+    public GameObject light;
+    public GameObject submarine;
+
+    public float maxLightIntensity = 0.8f;
+    public float minLightDepth = 20.0f;
+
+    public float minLightIntensity = 0.0f;
+    public float maxLightDepth = 100;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float depth = Mathf.Abs(submarine.transform.position.y);
+        float dN = Mathf.InverseLerp(minLightDepth, maxLightDepth, depth);
+        float nI = Mathf.Lerp(minLightIntensity, maxLightIntensity, dN);
+        light.GetComponent<Light>().intensity = nI;
+    }
+}
--- New Unity Project/Assets/Controller/UpgradeController.cs
+++ New Unity Project/Assets/Controller/UpgradeController.cs
@@ -14,7 +14,6 @@ public class UpgradeController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(scraps);
     }
 
     public void AddScrap(int amount)
