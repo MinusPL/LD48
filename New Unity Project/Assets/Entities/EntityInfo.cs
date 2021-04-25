d5daf9ce2be40934c4181e9c95446404b5be1ef3
Jakub Szatkowski <mistrzswiata.salata@gmail.com>
2021-04-25 12:33:14 +0200

    Scanner

---
+++ New Unity Project/Assets/Entities/EntityInfo.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+[System.Serializable]
+public class EntityInfo
+{
+    public int id;
+    public string name;
+    public string description;
+    public Texture texture;
+}
