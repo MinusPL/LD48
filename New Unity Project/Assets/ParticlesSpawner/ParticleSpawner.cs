26cd0322857110d8f9a75ff015bf072cfad96392
Jakub Szatkowski <mistrzswiata.salata@gmail.com>
2021-04-26 22:54:02 +0200

    Everything done

--- New Unity Project/Assets/ParticlesSpawner/ParticleSpawner.cs
+++ New Unity Project/Assets/ParticlesSpawner/ParticleSpawner.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class ParticleSpawner : MonoBehaviour
 {
@@ -13,39 +14,53 @@ public class ParticleSpawner : MonoBehaviour
 
     public GameObject rightSpawner;
 
-    public float maxSpawnHeight = -300;
+    public float fromSpawnHeight = -100;
+    public float toSpawnHeight = -300;
     public float maxRectSize = 100;
+    public float margin = 0.5f;
+    private void Start()
+    {
+    }
 
     void Update()
     {
+        Debug.Log(transform.position.y + maxRectSize / 2);
+        if (transform.position.y + maxRectSize / 2 > fromSpawnHeight || transform.position.y - maxRectSize / 2 < toSpawnHeight)
+        {
+            foreach (var ps in GetComponentsInChildren<ParticleSystem>())
+            {
+                ps.Stop();
+                ps.Clear();
+            }
+        }
         var player = GameObject.FindGameObjectWithTag("Player");
         var direction = player.transform.position - transform.position;
-        if (direction.x < -maxRectSize * 0.8 && leftSpawner == null)
+        if (direction.x < -maxRectSize/2 * margin && leftSpawner == null)
         {
-            leftSpawner = Instantiate(gameObject, transform.position + Vector3.left * maxRectSize / 2, Quaternion.identity);
+            leftSpawner = Instantiate(gameObject, transform.position + Vector3.left * maxRectSize, Quaternion.identity);
             leftSpawner.GetComponent<ParticleSpawner>().rightSpawner = gameObject;
-            leftSpawner.GetComponent<ParticleSpawner>().maxSpawnHeight = maxSpawnHeight;
+            leftSpawner.GetComponent<ParticleSpawner>().toSpawnHeight = toSpawnHeight;
             leftSpawner.GetComponent<ParticleSpawner>().maxRectSize = maxRectSize;
         }
-        if (direction.x > maxRectSize * 0.8 && rightSpawner == null)
+        if (direction.x > maxRectSize/2 * margin && rightSpawner == null)
         {
-            rightSpawner = Instantiate(gameObject, transform.position + Vector3.right * maxRectSize / 2, Quaternion.identity);
+            rightSpawner = Instantiate(gameObject, transform.position + Vector3.right * maxRectSize, Quaternion.identity);
             rightSpawner.GetComponent<ParticleSpawner>().leftSpawner = gameObject;
-            rightSpawner.GetComponent<ParticleSpawner>().maxSpawnHeight = maxSpawnHeight;
+            rightSpawner.GetComponent<ParticleSpawner>().toSpawnHeight = toSpawnHeight;
             rightSpawner.GetComponent<ParticleSpawner>().maxRectSize = maxRectSize;
         }
-        if (direction.y < -maxRectSize * 0.8 && downSpawner == null && transform.position.y + maxRectSize / 2 < maxSpawnHeight)
+        if (direction.y < -maxRectSize/2 * margin && downSpawner == null)
         {
-            downSpawner = Instantiate(gameObject, transform.position + Vector3.down * maxRectSize / 2, Quaternion.identity);
+            downSpawner = Instantiate(gameObject, transform.position + Vector3.down * maxRectSize, Quaternion.identity);
             downSpawner.GetComponent<ParticleSpawner>().upSpawner = gameObject;
-            downSpawner.GetComponent<ParticleSpawner>().maxSpawnHeight = maxSpawnHeight;
+            downSpawner.GetComponent<ParticleSpawner>().toSpawnHeight = toSpawnHeight;
             downSpawner.GetComponent<ParticleSpawner>().maxRectSize = maxRectSize;
         }
-        if (direction.y > maxRectSize * 0.8 && upSpawner == null)
+        if (direction.y > maxRectSize/2 * margin && upSpawner == null)
         {
-            upSpawner = Instantiate(gameObject, transform.position + Vector3.up * maxRectSize / 2, Quaternion.identity);
+            upSpawner = Instantiate(gameObject, transform.position + Vector3.up * maxRectSize, Quaternion.identity);
             upSpawner.GetComponent<ParticleSpawner>().downSpawner = gameObject;
-            upSpawner.GetComponent<ParticleSpawner>().maxSpawnHeight = maxSpawnHeight;
+            upSpawner.GetComponent<ParticleSpawner>().toSpawnHeight = toSpawnHeight;
             upSpawner.GetComponent<ParticleSpawner>().maxRectSize = maxRectSize;
         }
     }
